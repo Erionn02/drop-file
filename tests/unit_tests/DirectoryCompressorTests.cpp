03e616edeeb119515c68a7cc4d7a43097306552e
Erionn02 <jkjablonski@gmail.com>
2024-06-30 00:58:07 +0200

    Added more asserts to DirectoryCompressorTests

--- tests/unit_tests/DirectoryCompressorTests.cpp
+++ tests/unit_tests/DirectoryCompressorTests.cpp
@@ -2,6 +2,9 @@
 
 #include "DirectoryCompressor.hpp"
 
+#include <boost/range/combine.hpp>
+
+
 using namespace ::testing;
 
 struct DirectoryCompressorTests: public Test{
@@ -34,8 +37,36 @@ struct DirectoryCompressorTests: public Test{
             std::ofstream file{input_dir_path / "nested_dir" / "test_file.txt"};
             file << "Some other content";
         }
-        fs::create_directories(input_dir_path / "another_nested_dir");
+        fs::create_directories(input_dir_path / "another_nested_dir"); // todo add support for empty dirs
+    }
 
+    void assertDirectoriesEqual(const fs::path& dir1, const fs::path& dir2) {
+        auto dir_entries_1 = getSortedDirEntries(dir1);
+        auto dir_entries_2 = getSortedDirEntries(dir2);
+        ASSERT_EQ(dir_entries_1.size(), dir_entries_2.size());
+        for(auto [el1, el2]: boost::range::combine(dir_entries_1, dir_entries_2)) { // std::views::zip requires gcc 13 :(
+            ASSERT_EQ(el1.path().filename(), el1.path().filename());
+            ASSERT_EQ(el1.is_directory(), el2.is_directory());
+            ASSERT_EQ(el1.is_regular_file(), el2.is_regular_file());
+            if (el1.is_directory()) {
+                assertDirectoriesEqual(el1, el2);
+            } else {
+                ASSERT_EQ(getFileContent(el1), getFileContent(el2));
+            }
+        }
+    }
+
+    // normal iterating over directory does not guarantee any particular order of entries,
+    // therefore I have to sort it first
+    std::set<fs::directory_entry> getSortedDirEntries(const fs::path& dir) {
+        return {fs::directory_iterator(dir), fs::directory_iterator()};
+    }
+
+    std::string getFileContent(const fs::path& file_path) {
+        std::ifstream file{file_path, std::ios::binary};
+        std::stringstream ss;
+        ss << file.rdbuf();
+        return ss.str();
     }
 }; 
 
@@ -46,8 +77,9 @@ TEST_F(DirectoryCompressorTests, canCompressAndDecompress) {
     dc1.compress(archive_path);
 
     DirectoryCompressor dc2{output_dir_path};
-    dc1.decompress(archive_path);
+    dc2.decompress(archive_path);
     
     ASSERT_TRUE(fs::exists(archive_path));
     ASSERT_TRUE(fs::exists(output_dir_path));
+    assertDirectoriesEqual(input_dir_path, output_dir_path);
 }
